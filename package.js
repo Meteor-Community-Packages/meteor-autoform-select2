ace83f872048bb85071cdd8a40cd646e6ecd33fa
Eric Dobbertin <eric@dairystatedesigns.com>
2017-02-12 19:47:02 -0600

    Add autoform 6

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'aldeed:autoform-select2',
   summary: 'Custom select2 input type for AutoForm',
-  version: '2.0.2',
+  version: '2.0.3',
   git: 'https://github.com/aldeed/meteor-autoform-select2.git'
 });
 
@@ -9,7 +9,7 @@ Package.onUse(function(api) {
   api.use('templating@1.0.0');
   api.use('blaze@2.0.0');
   api.use('aldeed:template-extension@3.4.3');
-  api.use('aldeed:autoform@4.0.0 || 5.0.0');
+  api.use('aldeed:autoform@4.0.0 || 5.0.0 || 6.0.0');
   api.addFiles([
     'autoform-select2.html',
     'autoform-select2.js'
