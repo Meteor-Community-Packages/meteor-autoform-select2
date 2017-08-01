f96f8275e8dde81ddd33efb48a1db84a5ef3d71e
Eric Dobbertin <eric@dairystatedesigns.com>
2017-08-01 11:16:30 -0500

    Publish 3.0.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'aldeed:autoform-select2',
   summary: 'Custom select2 input type for AutoForm',
-  version: '3.0.0',
+  version: '3.0.1',
   git: 'https://github.com/aldeed/meteor-autoform-select2.git'
 });
 
